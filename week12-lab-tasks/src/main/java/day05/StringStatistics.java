9b48a122c43c3c96b0a28a987b6b6333f6acfae5
gbeatrix <gatibeatrix+github@gmail.com>
2022-01-14 16:25:18 +0100

    day05 abc rendezés

--- week12-lab-tasks/src/main/java/day05/StringStatistics.java
+++ week12-lab-tasks/src/main/java/day05/StringStatistics.java
@@ -1,15 +1,22 @@
 package day05;
 
+import java.text.Collator;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class StringStatistics {
     public static Map<Character, Integer> calcStat(String text) {
-        if(text == null){
+        if (text == null) {
             throw new IllegalArgumentException("text cannot be null");
         }
         char[] arr = text.toCharArray();
-        Map<Character, Integer> result = new TreeMap<>();
+        Map<Character, Integer> result = new TreeMap<>(new Comparator<>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return Collator.getInstance().compare("" + o1, "" + o2);
+            }
+        });
         for (char ch : arr) {
             if (result.containsKey(ch)) {
                 result.put(ch, result.get(ch) + 1);
